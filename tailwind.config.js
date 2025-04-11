89866e2bcbdc99cb50e0491352a0e1410387b0ce
webdev-raj <codewithsapnap@gmail.com>
2025-04-11 21:24:58 +0530

    finally done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        pop: ["Poppins","sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
